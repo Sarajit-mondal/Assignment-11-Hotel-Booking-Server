912d2036368097701973d9973c0268cddb6cb0ef
Your Name <sarajitmondal012@gmail.com>
2024-05-09 22:50:16 +0600

    mongodb connection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken');
@@ -17,6 +18,36 @@ app.use(cookieParser())
 app.get('/', (req, res) => {
   res.send('server is ranning....')
 })
+
+// mongodb database 
+// mongodb database 
+
+
+const uri =`mongodb+srv://${process.env.ADMIN_NAME}:${process.env.PASSWORD}@hotel-booking.a2yhjhv.mongodb.net/?retryWrites=true&w=majority&appName=Hotel-Booking` ;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    app.get('/all',(req,res)=>{
+      res.send("mongodb is working")
+    })
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// mongodb database 
+// mongodb database 
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
